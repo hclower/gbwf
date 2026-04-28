b639d0ffac0588afc2af7d28ae16fabd97280c6a
hclower <hclower@gmail.com>
2026-04-28 02:47:53 +0100

    Temp error state and title screen handlers, updated main, and high score manager. High score tester rolled into title screen state temprarily.

---
+++ inc/gbwf_utilities.h
@@ -0,0 +1,12 @@
+/**
+ * Utility functions for gbwf
+ */
+
+#ifndef _GBWF_UTILITIES_H
+#define _GBWF_UTILITIES_H
+
+#include <gb/gb.h>
+
+void waitForInput( void );
+
+#endif
---
+++ src/gbwf_utilities.c
@@ -0,0 +1,22 @@
+/**
+ * GBWF Utilities
+ */
+
+#include <gb/gb.h>
+
+/**
+ * Waits unitl the A or B buttons are pressed.
+ */
+void waitForInput( void ) {
+  uint8_t joypad_state;
+  
+  while( 1 )
+  {
+  	joypad_state = joypad();
+
+  	if( ( joypad_state & J_A ) ||
+  	    ( joypad_state & J_B )    )
+  		break;
+    vsync();
+  }
+}
