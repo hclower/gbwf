a891387fb4bb98561f3d415bea729d147593875e
hclower <hclower@gmail.com>
2026-05-09 11:17:42 +0100

    inc/gbwf.h
    Added doxygen comments, removed error manager header from gbwf

--- src/error_manager.c
+++ src/error_manager.c
@@ -9,9 +9,12 @@ errorCode_t global_err_code  = kNoError;
 uint16_t    global_err_data = 0;
 
 /**
- * Sets the two error variables
- *
+ * @brief Interface function to set the two error values
+ * @details Populates the two global error calues defined in error_manager.h
+ *          with the variables provided.
  * 
+ * @param error_code Error code to copy into global_err_code
+ * @param error_data Error data to copy into global_err_data
  */
 void setErrorVars( errorCode_t error_code, uint16_t error_data ) {
   global_err_code = error_code;
